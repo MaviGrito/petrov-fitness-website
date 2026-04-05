32af3ee80db30d18ce1478c817b1849fd3d70d88
Mauro Grisales <maurogrisales@gmail.com>
2026-04-04 19:01:01 -0500

    Remove sitemap plugin to fix Netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://petrovfitness.com.au',
   integrations: [
     tailwind(),
-    sitemap(),
   ],
 });
